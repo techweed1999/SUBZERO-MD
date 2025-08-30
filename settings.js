b19daba2a5ad035fdd63b09d3eccfc31465988e6
techweed1999 <weedtech56@gmail.com>
2025-08-30 11:26:37 -0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;2dkX0QiB#OoNomJqmEdDohKR_NwVj6kED5LFZwV5lJEzWoHUW5Xw", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
